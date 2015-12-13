808c4c4b2b2aab220a35498f02e0c73342ff8ce6
adventure-yunfei <adventure030@gmail.com>
2015-12-13 21:05:12 +0800

    move promise generator to Promise.js

--- src/Defer.js
+++ src/Defer.js
@@ -39,53 +39,13 @@ export default class Defer {
         });
     }
 
-    getPromise() {
-        if (!this._promise) {
-            this._promise = {
-                d: this,
-                then: (onFulfilled, onRejected) => {
-                    var defer = new Defer((resolve, reject) => {
-                        var handleHook = (hook, deferHooks, targetDeferState) => {
-                            var handlePromiseOutput = (output) => {
-                                if (!hook) {
-                                    (this.state === STATE_FULFILLED ? resolve : reject)(output);
-                                    return;
-                                }
-                                var result;
-                                try {
-                                    result = hook(output);
-                                } catch (e) {
-                                    reject(e);
-                                    return;
-                                }
-                                if (result === this._promise) {
-                                    reject(new TypeError('get the same promise object on chain'));
-                                } else if (result && typeof result.then === 'function') {
-                                    result.then((value) => {
-                                        resolve(value);
-                                    }, (reason) => {
-                                        reject(reason);
-                                    });
-                                } else {
-                                    resolve(result);
-                                }
-                            };
-                            if (this.state === STATE_PENDING) {
-                                deferHooks.push((output) => {
-                                    handlePromiseOutput(output);
-                                });
-                            } else if (this.state === targetDeferState) {
-                                handlePromiseOutput(this.output);
-                            }
-                        };
-                        handleHook(onFulfilled, this.fulfilledHooks, STATE_FULFILLED);
-                        handleHook(onRejected, this.rejectedHooks, STATE_REJECTED);
-                    });
-
-                    return defer.getPromise();
-                }
-            };
+    then(onFulfilled, onRejected) {
+        const {state, output, fulfilledHooks, rejectedHooks} = this;
+        if (state === STATE_PENDING) {
+            onFulfilled && fulfilledHooks.push(onFulfilled);
+            onRejected && rejectedHooks.push(onRejected);
+        } else {
+            (state === STATE_FULFILLED ? onFulfilled : onRejected)(output);
         }
-        return this._promise;
     }
 }
--- src/Promise.js
+++ src/Promise.js
@@ -1,6 +1,42 @@
 import Defer from './Defer';
 
-export default function Promise(fnResolver) {
-    var deferred = new Defer(fnResolver);
-    return deferred.getPromise();
+export default class Promise {
+    constructor(fnResolver) {
+        const defer = new Defer(fnResolver);
+
+        this.then = (onFulfilled, onRejected) => {
+            return new Promise((resolve, reject) => {
+                var handleDeferOutput = (hook, output, defaultHandler) => {
+                    if (!hook) {
+                        defaultHandler(output);
+                        return;
+                    }
+                    var result;
+                    try {
+                        result = hook(output);
+                    } catch (e) {
+                        reject(e);
+                        return;
+                    }
+                    if (result === this) {
+                        reject(new TypeError('get the same promise object on chain'));
+                    } else if (result && typeof result.then === 'function') {
+                        result.then((value) => {
+                            resolve(value);
+                        }, (reason) => {
+                            reject(reason);
+                        });
+                    } else {
+                        resolve(result);
+                    }
+                };
+
+                defer.then((value) => {
+                    handleDeferOutput(onFulfilled, value, resolve);
+                }, (reason) => {
+                    handleDeferOutput(onRejected, reason, reject);
+                });
+            });
+        };
+    }
 }
